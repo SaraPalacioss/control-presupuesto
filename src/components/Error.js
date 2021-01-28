15a3079e564758bbf1d8a955883c00aa5f61f9f2
sarastackhouse <sarapalacios7@gmail.com>
2021-01-28 19:41:14 +0100

    Budget validation ok

---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error = ({mensaje}) => (
+    <p className="alert alert-damger error">{mensaje}</p>
+)
+ 
+export default Error;
---
+++ src/components/Pregunta.js
@@ -0,0 +1,57 @@
+import React, {Fragment, useState}from 'react';
+import Error from "./Error"
+
+const Pregunta = () => {
+
+    //definir el state
+
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+
+    //función que lee el presupuesto
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value,10))
+    }
+
+    //submit para definit el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //validar
+        if(cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true)
+            return
+        }
+
+        //si se pasa la validación
+        guardarError(false);
+    }
+
+    return (  
+<Fragment>
+    <h2>Registra tu presupuesto</h2>
+
+    {error ? <Error mensaje="El presupuesto es incorrecto"/> : null}
+
+    <form
+        onSubmit={agregarPresupuesto}
+    >
+        <input
+            type="number"
+            className="u-full-width"
+            placeholder="Registra tu presupuesto"
+            onChange={definirPresupuesto}
+        />
+        <input
+            type="submit"
+            className="button-primary u-full-width"
+            value="Definir presupuesto"
+        />
+    </form>
+</Fragment>
+    );
+}
+ 
+export default Pregunta;
